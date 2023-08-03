88d554d8fb1eedd02788ca074fba779bb8d6f00e
DmytroKoba <d.koba81@gmail.com>
2023-08-03 12:15:02 -0400

    securing

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,31 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.core import serializers
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
+from rest_framework.decorators import api_view, permission_classes
+
+from .serializers import BookingSerializer, MenuSerializer
+from .models import Booking, Menu, MenuItem
 
 # Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
+
+class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Booking.objects.all()
